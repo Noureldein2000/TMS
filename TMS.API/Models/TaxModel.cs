8ff1ccccaa131c0d059b0df7692efd6788d05cba
ALAHLYMOMKN\nour.hussein <nour.hussein@momkn.com.eg>
2022-02-02 13:56:52 +0200

    create Controller Api EndPoint to Tax and DenomintionTaxes

---
+++ TMS.API/Models/TaxModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMS.API.Models
+{
+    public class TaxModel
+    {
+        public int ID { get; set; }
+        public int TaxesTypeID { get; set; }
+        public string TaxesTypeName { get; set; }
+        public string TaxRange { get; set; }
+        public decimal Taxes { get; set; }
+        public decimal AmountFrom { get; set; }
+        public decimal AmountTo { get; set; }
+        public int PaymentModeID { get; set; }
+        public int CreatedBy { get; set; }
+        public string PaymentModeName { get; set; }
+        public decimal Value { get; set; }
+        public bool Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
