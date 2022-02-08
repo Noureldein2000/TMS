ad4bde26385da6544bff394ee2fba590dfaca0c2
ALAHLYMOMKN\nour.hussein <nour.hussein@momkn.com.eg>
2022-02-08 16:04:47 +0200

    fix denomaiton name such as denomation ID by sending from index denomiantion Grid

--- TMS.API/Controllers/DenominationFeesController.cs
+++ TMS.API/Controllers/DenominationFeesController.cs
@@ -100,7 +100,6 @@ namespace TMS.API.Controllers
                 PaymentModeId = model.PaymentModeId,
                 PaymentMode = model.PaymentMode,
                 DenominationId = model.DenominationId,
-                DenominationFullName = model.DenominationFullName,
                 Range = model.Range
             };
         }
--- TMS.Services/Services/DenominationFeesService.cs
+++ TMS.Services/Services/DenominationFeesService.cs
@@ -60,7 +60,6 @@ namespace TMS.Services.Services
                 PaymentModeId = x.Fee.PaymentModeID,
                 PaymentMode = language == "en" ? x.Fee.PaymentMode.Name : x.Fee.PaymentMode.ArName,
                 DenominationId = x.DenominationID,
-                DenominationFullName = $"{x.Denomination.Service.Name} - {x.Denomination.Name}",
                 Range = $"{x.Fee.AmountFrom} - { x.Fee.AmountTo}",
                 CreationDate = x.CreationDate
             }).ToList();
